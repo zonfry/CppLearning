23e330adf53f1f88d068a53cd06df59d033f4672
zonfry <fabio.zonfrilli@hotmail.com>
2017-04-03 07:59:46 +0200

    The first commit

---
+++ 001_helloWorld/001_helloWorld/001_helloWorld.cpp
@@ -0,0 +1,12 @@
+// 001_helloWorld.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	std::cout << "Hello World\n";
+	return 0;
+}
+
